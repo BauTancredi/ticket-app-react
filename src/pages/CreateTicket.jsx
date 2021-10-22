ddcc2ae79ab54188d510bdb0fad02ead05f4d38f
BauTancredi <bautistatancredi@hotmail.com>
2021-10-22 09:51:19 -0300

    creating a new ticket

--- src/pages/CreateTicket.jsx
+++ src/pages/CreateTicket.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { Col, Row, Typography, Button } from "antd";
 import { DownloadOutlined } from "@ant-design/icons";
 import { useHideMenu } from "../hooks/useHideMenu";
+import { SocketContext } from "../context/SocketContext";
 
 const { Title, Text } = Typography;
 
 export const CreateTicket = () => {
   useHideMenu(true);
-  const newTicket = () => {};
+  const { socket } = useContext(SocketContext);
+  const [ticket, setTicket] = useState(null);
+
+  const newTicket = () => {
+    socket.emit("request-ticket", null, (ticket) => {
+      setTicket(ticket);
+    });
+  };
 
   return (
     <>
@@ -25,15 +33,18 @@ export const CreateTicket = () => {
           </Button>
         </Col>
       </Row>
-      <Row style={{ marginTop: 100 }}>
-        <Col span={14} offset={6} align="center">
-          <Text level={2}>Your number</Text>
-          <br />
-          <Text type="success" style={{ fontSize: 55 }}>
-            55
-          </Text>
-        </Col>
-      </Row>
+
+      {ticket && (
+        <Row style={{ marginTop: 100 }}>
+          <Col span={14} offset={6} align="center">
+            <Text level={2}>Your number</Text>
+            <br />
+            <Text type="success" style={{ fontSize: 55 }}>
+              {ticket.number}
+            </Text>
+          </Col>
+        </Row>
+      )}
     </>
   );
 };
